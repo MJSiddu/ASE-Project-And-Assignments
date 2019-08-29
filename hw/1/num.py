55b668fd8766c811e2c5f7d01804d51174f7c910
MJSiddu <siddumj21@gmail.com>
2019-08-29 00:37:29 -0400

    Comments added

--- hw/1/num.py
+++ hw/1/num.py
@@ -1,17 +1,21 @@
 import random
 import sys
 
+# Parent class Col
 class Col(object):
   def __init__(self):
     self.n = 0
 
+# Num class derived from Col
 class Num(Col):
+  # initialize
   def __init__(self):
     Col.__init__(self)
     self.mu = 0
     self.m2 = 0
     self.sd = 0
 
+  # add function
   def add(self, val):
     self.n += 1
     d1 = val - self.mu
@@ -26,6 +30,7 @@ class Num(Col):
 
     self.update_sd()
   
+  # delete function
   def delete(self, val):
     if (self.m2 < 0 or self.n < 2):
       self.sd = 0
@@ -40,12 +45,14 @@ class Num(Col):
     # Update sd
     self.update_sd()
 
+  # update function (helper)
   def update_sd(self):
     self.sd = (self.m2 / (self.n - 1)) ** 0.5
 
-
+# main block
 if __name__ == "__main__":
   nums = []
+  # generate 100 random numbers
   for _ in range(100):
     nums.append(random.randint(1,100))
   
@@ -55,6 +62,7 @@ if __name__ == "__main__":
   obj = Num()  
 
   for i in range(1, 101):
+    # invoke add
     obj.add(nums[i-1])
     if (i % 10 == 0):
       sd.append(obj.sd)
@@ -63,20 +71,25 @@ if __name__ == "__main__":
   for i in range(100, 9, -1):
     if (i % 10 == 0):
       index = int(i / 10) - 1
+      
+      # print results
       print ("-------------------------------")
       if (round(mean[index],2) == round(obj.mu, 2)):
         print ("Mean is equal - {}".format(mean[index]))
       if (round(sd[index],2) == round(obj.sd, 2)):
         print ("SD is equal - {}".format(sd[index]))
+        
+    # invoke delete
     obj.delete(nums[i-1])
 
-
+# Sym class derived from Col
 class Sym(Col):
   def __init__(self):
     Col.__init__(self)
     pass
-  
-class Sym(Col):
+
+# Some class derived from Col
+class Some(Col):
   def __init__(self):
     Col.__init__(self)
     pass
